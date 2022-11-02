ea3a99f5cba2de356e7fdb40d60985463ccfd8f5
Allie Soldau <alliesoldau@gmail.com>
2022-11-02 17:49:24 -0400

    final submit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.getElementsByTagName("main")[0];
+main.remove("main");
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Allie is the champion";
