8d7650255de4e149a31a007a31368bed16fd2abe
Allie Soldau <alliesoldau@gmail.com>
2022-11-01 22:08:07 -0400

    commit tree 715dbcea4a69c2464c1fcea5caad1c9a6a2fc951

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let main = document.getElementById('main');
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.textContent = 'Allie is the champion';
+document.body.append(newHeader);
+
